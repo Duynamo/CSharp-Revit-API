be1ea3926f28a1077ca82a73cf112f1ef999ca73
duynamo <vudinhduybm@gmail.com>
2024-08-05 21:57:01 +0700

    super

---
+++ _OLD/Lession 1/Lession 1/Program.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lession1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            #region MyRegion
+            //Console.Write("Duy");
+            //Console
+            //////Console.Write("Duy \n"); \\in ra man hinh se tu dong xuong dong
+            //Console.Write(123321);
+            ////Console.WriteLine();
+            ////Console.Read();
+
+            //Console.WriteLine("Duy \n"); //su dung ki tu dac biet de xuong dong
+            //Console.WriteLine("Banh mi"); //su dung lenh in ra man hinh co xuong dong
+            //Console.Write(123);// in ra gia tri nhung khong xuong dong
+            //Console.Write(Environment.NewLine); //su dung lenh xuong dong
+
+
+            //Console.ReadLine();// doi nguoi dung Enter
+
+
+            #endregion
+
+            #region MyRegion
+            //int a = 7;
+            //Console.WriteLine("a =" + a);
+            //Console.WriteLine("{0} {2} {1}", 1, 2, "Duy dep trai");
+            #endregion
+
+            #region console.readkey
+            //Console.WriteLine("Duy");
+            //Console.ReadKey();  // Readkey dung de xuat du lieu nhap tu ban phim ra man hinh.
+            //                    //Neu khong nhap gi thi mac dinh la false
+            //Console.ReadKey(false); // False:  hien thi phim da nhap ra man hinh
+            //Console.ReadKey(true); //Không hiển thị giá trị phím đã nhập ra màn hình
+
+
+
+            #endregion
+
+            #region console.readline()
+            //Console.WriteLine(Console.ReadLine()); // Câu lệnh sẽ được thực hiện theo thứ tự từ trong ra ngoài
+            //        //console.readline có tác dụng đọc dữ liệu từ bàn phím và sẽ ngừng khi gặp lệnh xuống hàng (enter)
+            //Console.ReadKey();// ngưng màn hình để xem kết quả
+
+
+            #endregion
+
+            #region Excercise
+
+            //Console.WriteLine("Vu Dinh Duy");
+            //Console.Write("Moi ban nhap ho va ten: ");
+            //Console.WriteLine("Ho va ten cua ban la: " + Console.ReadLine() + "Đẹp trai");
+            //Console.Write("Nhập quê quán: ");
+            //Console.WriteLine("Quê quán của you là: "+ Console.ReadLine());
+            //Console.ReadKey();
+
+
+
+
+            #endregion
+
+        }
+
+
+    }
+
+}
